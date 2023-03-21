8d135020cdfe5aefd9e411e6c4d07e73c48f48c6
MohammedDuke <hamoahmed8055@gmail.com>
2023-03-21 14:31:29 -0700

    is alpha script

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: check input of function
+ * Description - Print alphabet with new line at end
+ * Return: Always 0( Success)
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
