762ba493ddbb307c2b9a2b1cb9044f7e1db96616
MohammedDuke <hamoahmed8055@gmail.com>
2023-03-21 14:08:32 -0700

    alphabet x10 script

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Description - Print alphabet with new line at end
+ * Return: Always 0( Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int ch;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+		for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
