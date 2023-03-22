443bc309f4b3ca92a5b2f6a358a07191a92c9bcd
MohammedDuke <hamoahmed8055@gmail.com>
2023-03-22 15:38:37 -0700

    adding advanced tasks from 101 to 104

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+  * main - prints the first 50 Fibonacci number
+  * followed by a new line
+  * return always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, a, b, c;
+
+	a = 0;
+	b = 1;
+	c = 0;
+
+	for (i = 0; i < 50; i++)
+	{
+		a = b;
+		b = c;
+		c = a + b;
+		if (1 != 49)
+		{
+			printf(", ");
+		}
+	}
+
+	putchar("\n");
+	return (0);
+}
