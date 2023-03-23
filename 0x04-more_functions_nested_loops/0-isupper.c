c2a52c622a2d8f0b5ada5fecceba225e0044a4a9
MohammedDuke <hamoahmed8055@gmail.com>
2023-03-23 15:45:46 -0700

    fixing script

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,20 @@
 #include "main.h"
-
 /**
- *_isupper - check if c is upper or lower case
- *@c: input of alphabet
- *@return  1 if upppercase or 0 for lowercase
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
 */
-
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+
+
+	if (c >= 'A' && c <= 'Z')
 	{
-		return (1);
+	return (1);
 	}
 	else
-	{
-		return (0);
-	}
+{
+	return (0);
 }
 
+}
