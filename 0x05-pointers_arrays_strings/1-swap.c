9b485bbbb05babcd1fef3ede93adf2e293a9c371
MohammedDuke <hamoahmed8055@gmail.com>
2023-03-27 16:44:37 -0700

    swap script

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * swap_int - switch values of a and b
+  *
+  * @a: int parameter
+  * @b: int parameter
+  *
+  * return nothing
+  */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
